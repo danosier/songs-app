e62b1f577d5695e673f08328b3f2d5e612130ecd
Dan Osier <danielosier@gmail.com>
2024-09-04 00:36:18 -0400

    preparing for add-song route

--- front-end/app/components/main.tsx
+++ front-end/app/components/main.tsx
@@ -39,25 +39,23 @@ export default function MainComponent() {
   return (
     <>
       <main className="flex min-h-screen flex-col p-6">
-      <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
-      </div>
-      <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
-        <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
-          {songs && songs.map((song: Song, index: number) => {
-              return (
-                <div onClick={() => { setSelectedSong(song) }} key={ index }>
-                  <SongTitle title={ song.title } />
-                </div>
-              )
-          })}
+        <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
+          <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
+            {songs && songs.map((song: Song, index: number) => {
+                return (
+                  <div onClick={() => { setSelectedSong(song) }} key={ index }>
+                    <SongTitle title={ song.title } />
+                  </div>
+                )
+            })}
+          </div>
+          <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
+            {
+              selectedSong && <SongInfo song={ selectedSong } />
+            }
+          </div>
         </div>
-        <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-          {
-            selectedSong && <SongInfo song={ selectedSong } />
-          }
-        </div>
-      </div>
-    </main>
+      </main>
     </>
   )
 }
